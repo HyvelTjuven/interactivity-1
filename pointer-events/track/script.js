d981bd999ece7e7b774fddd4abaca3ec0b2acc33
Andrée Rehnberg <andree.rehnberg@gmail.com>
2018-01-15 15:33:19 +0100

    added to the pointer/track script for clarity

--- pointer-events/track/script.js
+++ pointer-events/track/script.js
@@ -7,20 +7,30 @@ function onDocumentReady() {
   el.addEventListener('pointerleave', onPointerLeave);
 }
 
-
 function onPointerUp(evt) {
   let el = getOrCreate(evt);
   el.classList.remove("down");
+  console.log(el.id + " up, " + "X: " + el.style.left + ", Y: " + el.style.top);
 }
 
 function onPointerDown(evt) {
   let el = getOrCreate(evt);
   el.classList.add("down");
+
+  // Position the element from its middle
+  let rect = el.getBoundingClientRect();
+  el.style.left = (evt.clientX - rect.width / 2) + "px";
+  el.style.top = (evt.clientY - rect.height / 2) + "px";
+
+  // Log ID and location of pointer
+  console.log(el.id + " down, " + "X: " + el.style.left + ", Y: " + el.style.top);
 }
 
 function onPointerLeave(evt) {
   let el = getOrCreate(evt);
   document.body.removeChild(el);
+  // Log ID and location of pointer
+  console.log(el.id + " left, " + "X: " + el.style.left + ", Y: " + el.style.top);
 }
 
 function onPointerMove(evt) {
@@ -28,20 +38,27 @@ function onPointerMove(evt) {
 
   // Position the element from its middle
   let rect = el.getBoundingClientRect();
-  el.style.left = (evt.clientX-rect.width/2) + "px";
-  el.style.top = (evt.clientY-rect.height/2) + "px";
+  el.style.left = (evt.clientX - rect.width / 2) + "px";
+  el.style.top = (evt.clientY - rect.height / 2) + "px";
 }
 
 // Returns an existing element for a pointer id, or makes a new one
 function getOrCreate(evt) {
+  // Get or create a new element with a unique ID
   const id = 'pointer-' + evt.pointerId;
   let el = document.getElementById(id);
-  if (el) return el;
-  el = document.createElement('div');
-  el.classList.add('pointer');
-  el.id = id;
-  document.body.appendChild(el);
-  return el;
+
+  // If an element with that ID already exists, return it to the function it was called from.
+  // Else if the element doesn't already exist, create and return it.
+  if (el) {
+    return el;
+  } else {
+    el = document.createElement('div');
+    el.classList.add('pointer');
+    el.id = id;
+    document.body.appendChild(el);
+    return el;
+  }
 }
 
 if (document.readyState != 'loading') onDocumentReady();
